ee35ca2664b86834224b4e2462e9f78af267ec2b
Deschain53 <jorgeadrianlucassanchez@gmail.com>
2021-07-15 19:23:13 -0500

    Intentado obtener el arreglo de productos en despliege git

--- src/helpers/getPrecio.js
+++ src/helpers/getPrecio.js
@@ -1,7 +1,8 @@
 //Hace la conexión con la API del mercado para los precios de un producto en especifico
 
 export const getPrecios = async(id) =>{
-    const header= `https://ronchon-vin-08117.herokuapp.com/`;
+    //const header= `https://ronchon-vin-08117.herokuapp.com/`;
+    const header='';
     const url = header +`https://www.simcompanies.com/api/v2/market/${encodeURI(id)}`;
     const resp = await fetch(url);
     const informacion = await resp.json();
